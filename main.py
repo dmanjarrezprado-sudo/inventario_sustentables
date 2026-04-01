a4095bcea3d3bfa47c9fa4b26abb77bbeafe0b6e
dvargaslinares-cpu <dvargaslinares@mail.weber.edu>
2026-03-31 19:12:40 -0600

    si/no

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 #Primero preguntar que desechan
 print("De los electronicos que tu tienes, que quieres desechar?")
 
+#el usuario nos da una parte
+#DELSIDE: pregunta si funcion o no
+
+if input("¿Funciona? (si/no)") == "si":
+    print("Puedes donarlo o venderlo")
+else:
+    print("Puedes reciclarlo o llevarlo a un punto limpio")
+
+
+
+
